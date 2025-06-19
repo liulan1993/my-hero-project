5e1c9baec10c2b50e22e5d849829640980a38e2b
liulan1993 <plj453369670@gmail.com>
2025-06-20 07:21:27 +0800

    11123

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -749,7 +749,6 @@ const ContainerAnimated = React.forwardRef<
       variants={filterVariants}
       transition={{
         ...SPRING_TRANSITION_CONFIG,
-        duration: 0.3,
         ...transition,
       }}
       {...props}
@@ -804,12 +803,12 @@ GalleryGridCell.displayName = "GalleryGridCell";
 const CtaWithGallerySection = () => {
   const IMAGES = [
     "https://images.unsplash.com/photo-1455849318743-b2233052fcff?q=80&w=2338&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1733680958774-39a0e8a64a54?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://images.unsplash.com/photo-1548783307-f63adc3f200b?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://images.unsplash.com/photo-1703622377707-29bc9409aaf2?q=80&w=2400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1526374965328-5f61d4dc18c5?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   ];
   return (
-    <section className="bg-transparent py-12">
+    <section className="bg-transparent py-24">
       <div className="mx-auto grid w-full max-w-6xl grid-cols-1 items-center gap-8 px-8 py-12 md:grid-cols-2">
         <ContainerStagger>
           <ContainerAnimated className="mb-4 block text-xs font-medium text-rose-500 md:text-sm">
