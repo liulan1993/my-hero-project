9a702b91bc630c9e2a9f5604fef0581010a35781
liulan1993 <plj453369670@gmail.com>
2025-06-20 19:39:19 +0800

    123

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1527,7 +1527,7 @@ TextMarquee.displayName = 'TextMarquee';
 // --- Marquee 组件的容器 Section ---
 const TextMarqueeSection = () => (
     <section className="py-24 md:py-32 w-full">
-         <div className="space-y-8">
+         <div className="space-y-2">
             <TextMarquee baseVelocity={-2} className='font-bold text-2xl text-blue-400'>
                Framer Motion · 
             </TextMarquee>
