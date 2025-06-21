b80dd07e09a2a4beef4578ce361e8b67baea59d0
liulan1993 <plj453369670@gmail.com>
2025-06-22 06:27:37 +0800

    123

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,16 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  // --- 这是本次唯一的修改 ---
-  // 我们在配置数组中添加一个新的对象，
-  // 专门用来定义我们的自定义规则。
-  {
-    rules: {
-      // 这条规则会禁用 ESLint 对 "any" 类型的报错，
-      // 从而解决我们之前遇到的编译阻塞问题。
-      "@typescript-eslint/no-explicit-any": "off",
-    },
-  },
 ];
 
 export default eslintConfig;
