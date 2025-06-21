1f33a4d28f5b2cf898cb86f949946938996e76e9
liulan1993 <plj453369670@gmail.com>
2025-06-22 06:56:51 +0800

    123

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* 您的配置项可以写在这里 */
+  /* For example:
+  images: {
+    domains: ['example.com'],
+  },
+  */
+};
+
+export default nextConfig;
