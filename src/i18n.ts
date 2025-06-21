d3821af59c2cc78c54375280d5aacf3f5289ec88
liulan1993 <plj453369670@gmail.com>
2025-06-22 05:50:33 +0800

    123

--- src/i18n.ts
+++ src/i18n.ts
@@ -6,12 +6,17 @@ const locales = ['en', 'zh', 'ja', 'ko', 'ru', 'fr'];
 
 export default getRequestConfig(async ({locale}) => {
   // 验证传入的 locale 是否有效
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   if (!locales.includes(locale as any)) {
       notFound();
   }
 
+  // --- 这是本次的最终修复 ---
+  // 1. 我们必须返回一个包含 locale 和 messages 的对象。
+  // 2. 我们使用类型断言 (as string) 来确保 locale 的类型是 string，
+  //    以解决之前遇到的 "string | undefined" 的类型冲突。
   return {
-    // 因为 i18n.ts 现在在 src 目录中，所以需要向上返回一级 (../) 才能找到 messages 文件夹
+    locale: locale as string, 
     messages: (await import(`../messages/${locale}.json`)).default
   };
 });
